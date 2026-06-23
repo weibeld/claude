60589dab67aed38281987f1bc65d7ffb7dced145
Daniel Weibel <danielmweibel@gmail.com>
2026-06-23 21:34:44 +0400

    Change order of elements in status line

--- statusline-command.sh
+++ statusline-command.sh
@@ -5,4 +5,4 @@ model=$(echo "$input" | jq -r '.model.display_name')
 dir=$(echo "$input" | jq -r '.workspace.current_dir')
 session=$(echo "$input" | jq -r '.session_id' | cut -c1-8)
 context=$(printf '%.0f' "$(echo "$input" | jq -r '.context_window.used_percentage // 0')")%
-echo "${dir} | ${session} | ${model} | ${context}"
+echo "${model} | ${session} | ${dir} | ${context}"
